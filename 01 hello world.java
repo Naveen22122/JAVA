d9909269e0de273066d695c0ae281010cd92df5b
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2024-06-12 13:40:31 +0530

    Create 01 hello world.java

---
+++ 01 hello world.java
@@ -0,0 +1,14 @@
+1) HELLO WORLD Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("KALKI");
+    }
+}
+      
+      output:
+      KALKI
+the class name can be anything and there should be class name
+        always matches to file name.  
+    1. the message inside the " " are string.
+----------------------
