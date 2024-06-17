af50ebd735bc643891239d7f69dd6c72f58ad3bd
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2024-06-17 15:56:45 +0530

    Create 2 if else  statement.java

---
+++ 2 if else  statement.java
@@ -0,0 +1,23 @@
+1) if else statement
+..........................................................................
+1) if else statement example:
+
+
+class main{
+public static void main(String[] args){
+int my_age = 20;
+int licence_application = 18;
+
+if (my_age >= licence_application){
+System.out.println("I am Eligible for driving.");
+}
+
+else {
+System.out.println("I am Not Eligible for driving.");
+}
+}
+}
+    OUTPUT:
+
+     I am Eligible for driving.
+...............................................................
