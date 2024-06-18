9396cf8e744355195166c801fffc08878485627d
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2024-06-18 20:49:36 +0530

    Update 02 if else  statement.java

--- 02 if else  statement.java
+++ 02 if else  statement.java
@@ -1,23 +1,64 @@
 1) if else statement
+2) if statement
+3) else if statement
 ..........................................................................
 1) if else statement example:
 
 
-class main{
-public static void main(String[] args){
-int my_age = 20;
-int licence_application = 18;
+        class main{
+        public static void main(String[] args){
+        int my_age = 20;
+        int licence_application = 18;
+        
+        if (my_age >= licence_application){
+        System.out.println("I am Eligible for driving.");
+        }
+        
+        else {
+        System.out.println("I am Not Eligible for driving.");
+        }
+        }
+        }
 
-if (my_age >= licence_application){
-System.out.println("I am Eligible for driving.");
-}
-
-else {
-System.out.println("I am Not Eligible for driving.");
-}
-}
-}
-    OUTPUT:
-
-     I am Eligible for driving.
+OUTPUT:
+             I am Eligible for driving.
 ...............................................................
+
+    2) if statement
+
+        class main{
+public static void main(String[]args){
+int x = 35;
+int y = 65;
+if (x<y){
+System.out.println("x is less than y");
+}
+}
+}
+
+    output:
+
+    x is less than y
+.....................................................................
+ 3) else if statement
+
+    class main{
+public static void main(String[]args){
+int score = 200;
+
+if ( score > 250 ){
+System.out.println("we will win the match");}
+else if ( score == 200 ){
+System.out.println("we can defend the score");}
+else {
+System.out.println(" we will lost the match");
+}
+}
+}
+
+  output:
+
+  we can defend the score
+........................................................
+
+    
