3c57a87be8234e3ad552a1e4b57f1c66bbd67d4b
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2024-06-19 17:43:49 +0530

    Create 04 While looop.java

---
+++ 04 While looop.java
@@ -0,0 +1,75 @@
+1) while loop statement
+2) do while loop statement
+3)while loop with if ststement
+..........................................................................
+1) ex of while loop
+
+          class main{
+          public static void main(String[]args){
+          
+          int students = 50;
+          while (students <= 55){
+          System.out.println(students);
+             students++;
+          }
+          }
+          }
+
+ output:
+          50
+          51
+          52
+          53
+          54
+          55
+........................................................................................
+
+ 2) ex of do while loop
+        
+        class main{
+        public static void main(String[]args){
+        
+        int students = 50;
+        do{
+        
+        System.out.println(students);
+           students++;
+        }
+        while (students <= 55);
+        }
+        }
+
+ output:
+        50
+        51
+        52
+        53
+        54
+        55
+  ...................................................................................
+3)while loop with if ststement
+
+         class main{
+          public static void main(String[]args){
+              int score = 0;
+              while(score<5){
+                  if (score<5){
+                      System.out.println("I lost");
+                      }else{
+                          System.out.println("I won");
+                      }
+                      score++;
+                  }        
+                }
+               }
+
+    output:   
+                  I lost
+                  I lost
+                  I lost
+                  I lost
+                  I lost
+....................................................................................................................
+                                
+                
+                               
